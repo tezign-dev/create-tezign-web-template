c2655c0cbc5908a35917af36f5183df54d0375f2
孙升 <sunsheng@sunshengdeMacBook-Pro.local>
2020-02-17 21:20:23 +0800

    update

--- src/pages/track-configs/locations.tsx
+++ src/pages/track-configs/locations.tsx
@@ -65,7 +65,7 @@ const patternUrlLabel = (
   <span>
     访问路径规则
     <Popover placement="rightTop" content={patternUrlLabelContent} title="可以做 URL 匹配，规则如下">
-      <Icon className="ml-4" type="guide"/>    
+      <Icon className="tz-action ml-4" type="guide"/>    
     </Popover> 
   </span>
 )
