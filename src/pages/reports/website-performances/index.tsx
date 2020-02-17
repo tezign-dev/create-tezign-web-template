8faf973cf6801f1856c8090a72744d14110a526f
孙升 <sunsheng@sunshengdeMacBook-Pro.local>
2020-02-17 09:59:08 +0800

    update

--- src/pages/reports/website-performances/index.tsx
+++ src/pages/reports/website-performances/index.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Tabs, Icon } from 'tezign-ui';
-import BasicLayout from '@/layouts/Basic'
-
-const TabPane = Tabs.TabPane
+import { Empty } from 'tezign-ui';
+// import each from 'commons.js/functions/each'
 
 export default function (props: any) {
+  // each([1,3,4])
   return (
-    <div>
-      <Tabs defaultActiveKey="1" type="radio-card">
-        <TabPane tab={<div><Icon type="profile"/>个人简介</div>} key="1">weirth Content of tab 1</TabPane>
-        <TabPane tab={<><Icon type="project-ex"/>项目经历</>} disabled key="2">Content of tab 2</TabPane>
-        <TabPane tab={<><Icon type="work-ex"/>工作经历</>} key="3">Content of tab 3</TabPane>
-        <TabPane tab={<><Icon type="learning-ex"/>学习经历</>} key="4">Content of tab 3</TabPane>
-      </Tabs>
-    </div>
+    <Empty size="large" image={Empty.IMAGES.NO_DATA} description="暂无内容" height={400}/>
   );
 }
